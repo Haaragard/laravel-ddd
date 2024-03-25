81402dbabe4a78b27b43dbdd3285a61bfd77169f
haaragard <haaragard@gmail.com>
2024-03-25 00:08:37 -0300

    feat: add ForceJsonMiddleware.php

---
+++ app/Infrastructure/Http/Middleware/ForceJsonMiddleware.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Infrastructure\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ForceJsonMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $request->headers->set('Content-Type', 'application/json');
+        $request->headers->set('Accept', 'application/json');
+
+        return $next($request);
+    }
+}
