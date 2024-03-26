d7671e4d3a3d070931983f462fa4631384d4f8ca
haaragard <haaragard@gmail.com>
2024-03-25 23:50:05 -0300

    chore: update resource data

--- app/Infrastructure/Http/Resource/User/StoreUserResponse.php
+++ app/Infrastructure/Http/Resource/User/StoreUserResponse.php
@@ -4,9 +4,19 @@ namespace App\Infrastructure\Http\Resource\User;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
-use Illuminate\Http\Resources\Json\ResourceResponse;
 
 class StoreUserResponse extends JsonResource
 {
-    //
+    public function toArray(Request $request)
+    {
+        if (is_null($this->resource)) {
+            return [];
+        }
+
+        return [
+            'data' => is_array($this->resource)
+                ? $this->resource
+                : $this->resource->toArray()
+        ];
+    }
 }
